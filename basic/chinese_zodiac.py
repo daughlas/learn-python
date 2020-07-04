a9472b4f35df5392aac83451ce620930516a8319
daughlas <daughlas@gmail.com>
2020-07-04 10:02:12 +0800

    init commit

--- basic/chinese_zodiac.py
+++
@@ -1,4 +0,0 @@
-chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
-
-for year in range(2000, 2018):
-    print('%s 年的生肖是 %s' %(year, chinese_zodiac[year % 12]))
--- basic/random_test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# for i in range(100):
-#     print(random.randint(1, 5))
-
-
-print(random.randint(1, 5))
-print(random.choice(('aa', 'bb', 'cc')))
